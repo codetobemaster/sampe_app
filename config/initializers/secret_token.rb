26db6f66f6d63917ab59e01ed1dc0a08117edb35
tannv85 <tannv85.@gmail.com>
2012-10-01 11:15:52 +0700

    innitial appp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampeApp::Application.config.secret_token = '24892afa39358cc23f27f322754c00a2ac12c362181f1a23c766fdc97c597ac22810e9bc9db20f1d5ebdd00e36b84227df7a6ea4411b0b95c3cd9433f2c73a39'
